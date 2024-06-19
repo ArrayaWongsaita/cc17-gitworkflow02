3bc30239dcab61ed4389d622b6b38d51cbfabc44
ArrayaWongsaita <wongsaita.a@gmail.com>
2024-06-19 14:06:54 +0700

    register

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 
 const express = require('express');
 const errorMiddleware = require('./middlewares/error-middleware');
-const notFoundMiddleware = require('./middlewares/notFound-middleware')
+const notFoundMiddleware = require('./middlewares/notFound-middleware');
+const { register } = require('module');
 
 
 const app = express();
 
 
-
+app.use('/register',register)
 
 
 app.use(errorMiddleware);
