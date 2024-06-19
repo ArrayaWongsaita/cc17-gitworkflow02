63cd051b2407dcabfeb4dfb41ec492785f90efef
SiraphobCh <spcc.321@gmail.com>
2024-06-19 14:07:20 +0700

    login

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
-
 const express = require('express');
 const errorMiddleware = require('./middlewares/error-middleware');
-const notFoundMiddleware = require('./middlewares/notFound-middleware')
-
+const notFoundMiddleware = require('./middlewares/notFound-middleware');
+const { login } = require('./routes/auth-route');
 
 const app = express();
 
-
-
-
+app.use('/login', login);
 
 app.use(errorMiddleware);
-app.use(notFoundMiddleware)
+app.use(notFoundMiddleware);
 
-const port = 8000
-app.listen(port, ()=>console.log('server on ',port))
-
+const port = 8000;
+app.listen(port, () => console.log('server on ', port));
