558f5a557d90db25a9daedd62b2433f497fea665
ArrayaWongsaita <wongsaita.a@gmail.com>
2024-06-19 12:23:25 +0700

    add feature/notfound

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const notFoundMiddleware = require('./middlewares/notFound-middleware')
 
 const app = express()
+
+
+app.use(notFoundMiddleware)
 
 const port = 8000
 app.listen(port, ()=>console.log('server on ',port))
